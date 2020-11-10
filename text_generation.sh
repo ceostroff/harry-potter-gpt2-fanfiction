1e8cd9881fd60ea16b1f89c8daacb0dc3c777987
Martín González <m@martingonzalez.net>
2020-11-10 21:30:18 +0100

    Use hosted model

--- text_generation.sh
+++ text_generation.sh
@@ -1,7 +1,7 @@
 python run_generation.py \
     --model_type=gpt2 \
-    --model_name_or_path=model/ \
-    --length=100 \
+    --model_name_or_path=ceostroff/harry-potter-gpt2-fanfiction \
+    --length=60 \
     --temperature=0.7 \
     --repetition_penalty=3 \
-    --num_return_sequences=20
+    --num_return_sequences=10
